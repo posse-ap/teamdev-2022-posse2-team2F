83c767af7cb77a3a27a571d75770fe49e64caeda
kohei23n <kohei.s.n@gmail.com>
2022-04-16 17:23:46 +0900

    ログイン＋新規登録機能終了

--- mysql/docker-entrypoint-initdb.d/init.sql
+++ mysql/docker-entrypoint-initdb.d/init.sql
@@ -9,11 +9,13 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   email VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
+  password_conf VARCHAR(255) NOT NULL
 );
 
 INSERT INTO
   users
 SET
-  email = 'test@posse-ap.com',
-  password = sha1('password')
+  email = 'admin@boozer.com',
+  password = sha1('password'),
+  password_conf = sha1('password')
