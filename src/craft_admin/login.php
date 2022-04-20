05ba7fa2483e78cf8185cacf1f29e9f0979a6ff3
kohei23n <kohei.s.n@gmail.com>
2022-04-20 12:04:34 +0900

    見た目とりあえず終了

--- src/craft_admin/login.php
+++ src/craft_admin/login.php
@@ -35,7 +35,7 @@ if (isset($_POST['login'])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="login.css">
+  <link rel="stylesheet" href="login.css"> 
   <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
   <title>管理者ログイン</title>
 </head>
