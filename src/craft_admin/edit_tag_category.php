b9b4ab0cf1d0b89548597d41ef744df398825c2e
kohei23n <kohei.s.n@gmail.com>
2022-05-01 13:54:15 +0900

    クラス名修正完了

--- src/craft_admin/edit_tag_category.php
+++ src/craft_admin/edit_tag_category.php
@@ -65,8 +65,8 @@ if (isset($_GET['id'])) {
       <div class="util_sidebar_button">
         <a class="util_sidebar_link" href="/craft_admin/add_agent.php">エージェント追加</a>
       </div>
-      <div class="util_sidebar_button util_sidebar_button-selected">
-        <a class="util_sidebar_link util_sidebar_link-selected" href="">タグ編集・追加</a>
+      <div class="util_sidebar_button util_sidebar_button--selected">
+        <a class="util_sidebar_link util_sidebar_link--selected" href="">タグ編集・追加</a>
       </div>
       <div class="util_sidebar_button">
         <a class="util_sidebar_link" href="">ユーザー用サイトへ</a>
--- src/craft_admin/tag.php
+++ src/craft_admin/tag.php
@@ -32,8 +32,8 @@ $categories = $stmt->fetchAll();
       <div class="util_sidebar_button">
         <a class="util_sidebar_link" href="/craft_admin/add_agent.php">エージェント追加</a>
       </div>
-      <div class="util_sidebar_button util_sidebar_button-selected">
-        <a class="util_sidebar_link util_sidebar_link-selected" href="/craft_admin/tag.php">タグ編集・追加</a>
+      <div class="util_sidebar_button util_sidebar_button--selected">
+        <a class="util_sidebar_link util_sidebar_link--selected" href="/craft_admin/tag.php">タグ編集・追加</a>
       </div>
       <div class="util_sidebar_button">
         <a class="util_sidebar_link" href="">ユーザー用サイトへ</a>
