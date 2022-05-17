acb25a0a0644f03a46b2d50c8d6e38686f0a3f61
kohei23n <kohei.s.n@gmail.com>
2022-05-17 10:53:21 +0900

    エージェント用学生一覧が表示されないエラー解消

--- src/craft_admin/students_info.php
+++ src/craft_admin/students_info.php
@@ -8,10 +8,10 @@ require('../dbconnect.php');
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 
 <body>
@@ -212,6 +212,7 @@ require('../dbconnect.php');
             </form>
             </div>
         </div>
+    </div>
 
 
 <?php require('../_footer.php'); ?>
