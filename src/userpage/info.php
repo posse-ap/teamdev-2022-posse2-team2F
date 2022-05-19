7f9b6a4d5d6d92618d26f1735203de9256179658
lisa <lisa.terashima72@gmail.com>
2022-05-19 22:11:50 +0900

    残るはcss

---
+++ src/userpage/info.php
@@ -0,0 +1,31 @@
+<?php
+require('../dbconnect.php');?>
+
+
+<?php require('../_header.php'); ?>
+
+<div class="container">
+  <div class="heading_wrapper">
+    <h2 class>エージェント詳細</h2>
+  </div>
+  <div class="info_wrapper">
+    <div class="info_wrapper-name">
+      <h2>エージェントA</h2>
+    </div>
+    <div class="info_inner">
+      <p>単なる有名企業ではなく<br>「働きやすい企業を選ぶ」</p>
+      <img src="https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqGxuQACxqi0fkbGNWy3I1FNVBepEwDgN39TsXY8C5PI3LIRcl5vUjWn5W9MG8-P9TrTMwqx4j6JcYDIcqw_MaPg4YSsMhnpaCpUeVPyG9yk9XCth2cBr0pHYs9L6kGvWtxtyBPfrGyo5qJCcXxPGa6NEuKtVajSOCYe7GRgJtAHcL/maxresdefault.jpg?errorImage=false" class="img_agent" alt="">
+      <div class="info_innner-tag"></div>
+      <ul class="info_inner-content">
+        <li>マッチング力に強みを持つ</li>
+        <li>徹底的なカウンセリングを行い、<br>厳選して求人を紹介</li>
+      </ul>
+      <div class="btn_wrapper">
+        <div class="btn-more">詳細を見る</div>
+        <div class="btn-register">申し込む</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php require('../_footer.php'); ?>
