f4bbb74eacfdbab59d1d5bb82c6d1aeb519c355b
lisa <lisa.terashima72@gmail.com>
2022-05-21 22:15:37 +0900

    まいなび新卒紹介

--- src/userpage/info.php
+++ src/userpage/info.php
@@ -10,18 +10,29 @@ require('../dbconnect.php');?>
   </div>
   <div class="info_wrapper">
     <div class="info_wrapper-name">
-      <h2>エージェントA</h2>
+      <h2 class="info-name">まいなび新卒紹介</h2>
     </div>
     <div class="info_inner">
-      <p>単なる有名企業ではなく<br>「働きやすい企業を選ぶ」</p>
-      <img src="https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqGxuQACxqi0fkbGNWy3I1FNVBepEwDgN39TsXY8C5PI3LIRcl5vUjWn5W9MG8-P9TrTMwqx4j6JcYDIcqw_MaPg4YSsMhnpaCpUeVPyG9yk9XCth2cBr0pHYs9L6kGvWtxtyBPfrGyo5qJCcXxPGa6NEuKtVajSOCYe7GRgJtAHcL/maxresdefault.jpg?errorImage=false" class="img_agent" alt="">
-      <div class="info_innner-tag"></div>
-      <ul class="info_inner-content">
-        <li>マッチング力に強みを持つ</li>
-        <li>徹底的なカウンセリングを行い、<br>厳選して求人を紹介</li>
-      </ul>
-      <div class="btn_wrapper">
-        <div class="btn-more">詳細を見る</div>
+      <div class="info_inner-box">
+        <div class="info_inner-leftbox">
+          <img src="https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqGxuQACxqi0fkbGNWy3I1FNVBepEwDgN39TsXY8C5PI3LIRcl5vUjWn5W9MG8-P9TrTMwqx4j6JcYDIcqw_MaPg4YSsMhnpaCpUeVPyG9yk9XCth2cBr0pHYs9L6kGvWtxtyBPfrGyo5qJCcXxPGa6NEuKtVajSOCYe7GRgJtAHcL/maxresdefault.jpg?errorImage=false" class="img_agent" alt="">
+          <div class="info_inner-compararison"></div>
+        </div>
+        <div class="info_inner-rightbox">
+          <h3 class="info_inner-rightbox_catchcopy">国内トップクラスのデータベースで<br>幅広い求人紹介を見つけられる！</h3>
+          <p class="info_inner-rightbox_context">大手人材会社のマイナビが運営するこちらの就活エージェント。人材業界最大手の企業だからこそ、企業データを豊富に持っており紹介できる企業が幅広いといえます。東京・名古屋・大阪・京都と4つの拠点があるうえ、マイナビ全体の営業担当が全国各地で活動。地方の就職をサポートする体制が整っていることが魅力です。また、マイナビは展開しているすべてのサービスで面談を徹底していることが特徴。時間をかけて面談がしたい人向きのエージェントです。</p>
+          <div class="info_inner-point">
+            <h4 class="info_inner-point_title">ポイントまとめ</h4>
+            <ul class="info_inner-point_list">
+              <li>大手ゆえ就職先選択肢が多い！</li>
+              <li>若手育成研修も実施！</li>
+              <li>先輩方の就活コラムも参照できる！</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="btn">
+        <div class="btn-fav">お気に入りに追加</div>
         <div class="btn-register">申し込む</div>
       </div>
     </div>
